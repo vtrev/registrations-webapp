c51a7d9e4e3f8103131fe38dc474e90b66601ccc
vusi <baloyi.trev@gmail.com>
2018-09-06 15:42:51 +0200

    cleaned out comments

--- test/Reg-Factory.test.js
+++ test/Reg-Factory.test.js
@@ -6,7 +6,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:8423@localhost:5432/registrations';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/registrations';
 
 const pool = new Pool({
     connectionString
