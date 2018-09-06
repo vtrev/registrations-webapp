e1107a2a164dc2958fdeba0e6b0251e6cac31bad
vusi <baloyi.trev@gmail.com>
2018-09-06 14:45:33 +0200

    fixed travis.yml

--- test/Reg-Factory.test.js
+++ test/Reg-Factory.test.js
@@ -21,7 +21,7 @@ if (process.env.DATABASE_URL && !local) {
     useSSL = true;
 }
 // which db connection to use
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:8423@localhost:5432/registrations';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/registrations';
 
 const pool = new Pool({
     connectionString,
